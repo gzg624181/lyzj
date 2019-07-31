7c28c0572ff87f2d92c1cc19a898f4c20cbd5e9c
ECHO <704970189@qq.com>
2019-07-31 10:00:37 +0800

    mac 2019-07-31

---
+++ templates/admin/comment.php
@@ -0,0 +1,155 @@
+<?php require_once(dirname(__FILE__).'/inc/config.inc.php'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>评论管理</title>
+<link href="templates/style/admin.css" rel="stylesheet" type="text/css" />
+<link href="templates/style/menu1.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="templates/js/jquery.min.js"></script>
+<script type="text/javascript" src="templates/js/forms.func.js"></script>
+<script type="text/javascript" src="layer/layer.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<script>
+function ord(OrderId)
+{
+  layer.open({
+  type: 2,
+  title: '订单详情：',
+  maxmin: true,
+  shadeClose: true, //点击遮罩关闭	层
+  area : ['900px' , '600px'],
+  content: 'ordershow.php?OrderId='+OrderId,
+  });
+
+}
+function GetSearchs(){
+var keyword= document.getElementById("keyword").value;
+if($("#keyword").val() == "")
+{
+ layer.alert("请输入搜索内容！",{icon:0});
+ $("#keyword").focus();
+ return false;
+}
+window.location.href='comment.php?keyword='+keyword;
+}
+function changestatus(status,id){
+	var ajax_url='comment_ajax.php?id='+id+'&status='+status;
+   //alert(ajax_url);
+	$.ajax({
+    url:ajax_url,
+    type:'get',
+	data: "data" ,
+	dataType:'html',
+    success:function(data){
+     document.getElementById("changestatus"+id).innerHTML = data;
+    } ,
+	error:function(){
+       alert('error');
+    }
+	});
+	}
+  function showimg(orderid,Id){
+    var ajax_url='ajax_imgs.php?orderid='+orderid;
+  // alert(ajax_url);
+	$.ajax({
+    url:ajax_url,
+    type:'get',
+	data: "data" ,
+	dataType:'html',
+    success:function(data){
+	var div1 = document.getElementById("checkimages"+Id);
+       div1.style.display="";
+     document.getElementById("checkimages"+Id).innerHTML = data;
+    } ,
+	error:function(){
+       alert('error');
+    }
+	});
+  }
+  function outimg(orderid,Id){
+
+	var div1 = document.getElementById("checkimages"+Id);
+     div1.style.display="none";
+
+  }
+function reply(id){
+	var recomment=document.getElementById('recomment').value;
+	window.location.href="comment_save.php?id="+id+"&action=reply"+"&recomment="+recomment;
+	}
+</script>
+</head>
+<body>
+<?php
+$adminlevel=$_SESSION['adminlevel'];
+$keyword = isset($keyword) ? $keyword : '';
+?>
+<div class="topToolbar"> <span class="title">所有评论</span> <a href="javascript:location.reload();" class="reload"><?php  echo $cfg_reload; ?></a></div>
+<div class="toolbarTab" style="margin-bottom:5px;">
+  <div id="search" class="search"> <span class="s">
+<input name="keyword" id="keyword" type="text" class="number" style="font-size:11px;" placeholder="请输入旅行社名称或者导游姓名" title="请输入旅行社名称或者导游姓名" />
+		</span> <span class="b"><a href="javascript:;" onclick="GetSearchs();"></a></span></div>
+	<div class="cl"></div>
+</div>
+<form name="form" id="form" method="post" action="comment_save.php">
+	<table width="100%" border="0" cellpadding="0" cellspacing="0" class="dataTable">
+		<tr align="left" class="head">
+			<td width="1%" height="31" align="center"><input type="checkbox" name="checkid" id="checkid" onclick="CheckAll(this.checked);"></td>
+			<td width="5%" align="center">旅行社头像</td>
+			<td width="9%" align="center">旅行社名称</td>
+			<td width="34%">评论内容</td>
+			<td width="9%">评价几星</td>
+			<td width="15%">导游姓名</td>
+			<td width="15%">评论时间</td>
+			<td width="3%" align="left">操作</td>
+		</tr>
+		<?php
+    if($keyword!=""){
+    $dopage->GetPage("SELECT b.images,b.company,a.content,a.star,a.addtime,a.id,a.gid,c.name FROM `pmw_comment` a left join pmw_agency b on b.id=a.aid left join  pmw_guide c on a.gid=c.id where b.company like '%$keyword%' or c.name like '%$keyword%'",15);
+    }else{
+		$dopage->GetPage("SELECT b.images,b.company,a.content,a.star,a.addtime,a.id,a.gid,c.name FROM `pmw_comment` a left join pmw_agency b on b.id=a.aid left join  pmw_guide c on a.gid=c.id",15);
+    }
+		while($row = $dosql->GetArray())
+		{
+
+      if(check_str($row['images'],"https")){
+        $images = $row['images'];
+      }else{
+        $images =$cfg_weburl.$row['images'];
+      }
+
+		?>
+
+		<tr align="left" class="dataTr">
+			<td height="42" align="center"><input type="checkbox" name="checkid[]" id="checkid[]" value="<?php  echo $row['Id']; ?>" /></td>
+			<td align="center" valign="middle"><img style="height: 50px;padding: 5px;" src="<?php  echo $images;?>" /></td>
+			<td align="center" valign="middle"><?php  echo $row['company'];?></td>
+      <td align="center" ><?php  echo $row['content'];?></td>
+			<td align="center" ><?php  echo $row['star'];?> </td>
+			<td align="center" ><?php echo $row['name'];?></td></td>
+			<td align="center" ><?php  echo date("Y-m-d H:i:s",$row['addtime']);?></td>
+			<td align="center">
+            <!-- <div id="jsddm"><a title="回复评论内容" href="javascript:vod(0)" onclick="reply('<?php echo $row['id'];?>');"><i class="fa fa-reply-all" aria-hidden="true"></i></a></div> -->
+
+            <div id="jsddm"><a title="删除" href="comment_save.php?action=del2&id=<?php  echo $row['id']; ?>" onclick="return ConfDel(0);"><i class="fa fa-trash fa-lg fa-fw" aria-hidden="true"></i></a></div>
+
+            </td>
+		</tr>
+
+		<?php
+		}
+		?>
+	</table>
+</form>
+<?php
+
+//判断无记录样式
+if($dosql->GetTotalRow() == 0)
+{
+	echo '<div class="dataEmpty">暂时没有相关的记录</div>';
+}
+?>
+<div class="bottomToolbar"><span class="selArea"><span>选择：</span> <a href="javascript:CheckAll(true);">全部</a> - <a href="javascript:CheckAll(false);">无</a> - <a href="javascript:DelAllNone('promotion_save.php');" onclick="return ConfDelAll(0);">删除</a></span></div>
+<div class="page"> <?php echo $dopage->GetList(); ?> </div>
+</body>
+</html>
---
+++ templates/api/get_search_scenic/index.php
@@ -0,0 +1,157 @@
+<?php
+    /**
+	   * 链接地址：get_search_scenic  搜索景点 关键字模糊搜索
+	   *
+     * 下面直接来连接操作数据库进而得到json串
+     *
+     * 按json方式输出通信数据
+     *
+     * @param unknown $State 状态码
+     *
+     * @param string $Descriptor  提示信息
+     *
+	   * @param string $Version  操作时间
+     *
+     * @param array $Data 数据
+     *
+     * @return string
+     *
+     * @提供返回参数账号  keyword=>    title
+     */
+require_once("../../include/config.inc.php");
+$Data = array();
+$Version=date("Y-m-d H:i:s");
+if(isset($token) && $token==$cfg_auth_key){
+    $r=$dosql->GetOne("SELECT imagesurl FROM pmw_share  where id=3");
+    $cfg_default = $r['imagesurl'];
+
+    if(isset($keyword)){
+
+    $dosql->Execute("SELECT id,names,label,solds,types,flag,lowmoney,remarks,level,picarr FROM pmw_ticket where names like '%$keyword%' and  checkinfo=1 order by id desc ");
+
+    $num=$dosql->GetTotalRow();//获取数据条数
+
+    }else{
+
+    $num=0;
+
+    }
+
+      if($num > 0){  //搜索结果
+
+        for($i=0;$i<$num;$i++){
+         $row = $dosql->GetArray();
+         $Data['list'][$i]=$row;
+
+         $picarr=stripslashes($row['picarr']);
+         if($picarr==""){
+         $picarrTmp=array("0"=>$cfg_weburl."/".$cfg_default);
+         $picarr = json_encode($picarrTmp);
+         }else{
+         $picarr=GetPic($picarr, $cfg_weburl);
+         }
+         $Data['list'][$i]['picarr']=$picarr;
+        }
+
+        //如果有搜索的内容的时候，则将数据保存到搜索历史表里面
+         if(isset($openid)){
+           //判断搜索历史表里面是否有相同的搜索关键字
+
+           $r=$dosql->GetOne("SELECT keyword FROM pmw_searchlist where openid='$openid' and type=1");
+
+           if(!is_array($r)){
+             //往搜索历史记录表里面添加搜索记录
+             $posttime = time();
+             $sql="INSERT INTO  `#@__searchlist` (keyword,openid,posttime,type) values ('$keyword','$openid',$posttime,1)";
+              $dosql->ExecNoneQuery($sql);
+           }
+           //查询历史搜索记录（显示最新的五条搜索记录）
+
+           $two=2;
+           $dosql->Execute("SELECT * FROM `#@__searchlist` where openid='$openid'  and type=1 order by id desc limit  5",$two);
+           for($i=0;$i<$dosql->GetTotalRow($two);$i++){
+            $show=$dosql->GetArray($two);
+            $Data['searchlist'][$i]=$show;
+           }
+         }
+
+         //默认推荐四条景点的数据
+         $four=4;
+         $dosql->Execute("SELECT * FROM pmw_ticket where checkinfo=1 order by rand() limit 4",$four);
+         for($i=0;$i<$dosql->GetTotalRow($four);$i++){
+          $row = $dosql->GetArray($four);
+          $Data['recommand'][$i]=$row;
+
+          $picarr=stripslashes($row['picarr']);
+          if($picarr==""){
+          $picarrTmp=array("0"=>$cfg_weburl."/".$cfg_default);
+          $picarr = json_encode($picarrTmp);
+          }else{
+          $picarr=GetPic($picarr, $cfg_weburl);
+          }
+          $Data['recommand'][$i]['picarr']=$picarr;
+
+         }
+      $State = 1;
+      $Descriptor = '搜索数据查询成功！';
+      $result = array (
+                  'State' => $State,
+                  'Descriptor' => $Descriptor,
+                  'Version' => $Version,
+                  'Data' => $Data
+                   );
+      echo phpver($result);
+      }else{
+
+    //  当搜索数据内容为空的时候，随机选出4个
+
+    $six=6;
+    $dosql->Execute("SELECT * FROM pmw_ticket where checkinfo=1 order by rand() limit 4",$six);
+    for($i=0;$i<$dosql->GetTotalRow($six);$i++){
+     $row = $dosql->GetArray($six);
+     $Data['recommand'][$i]=$row;
+
+     $picarr=stripslashes($row['picarr']);
+     if($picarr==""){
+     $picarrTmp=array("0"=>$cfg_weburl."/".$cfg_default);
+     $picarr = json_encode($picarrTmp);
+     }else{
+     $picarr=GetPic($picarr, $cfg_weburl);
+     }
+     $Data['recommand'][$i]['picarr']=$picarr;
+
+    }
+    if(isset($openid)){
+     $five=5;
+     $dosql->Execute("SELECT * FROM `#@__searchlist` where openid='$openid' and type=1 order by id desc limit 5",$five);
+     for($i=0;$i<$dosql->GetTotalRow($five);$i++){
+     $go=$dosql->GetArray($five);
+     $Data['searchlist'][$i]=$go;
+                    }
+     }
+
+        $State = 0;
+        $Descriptor = '搜索数据为空！';
+        $result = array (
+                    'State' => $State,
+                    'Descriptor' => $Descriptor,
+                    'Version' => $Version,
+                    'Data' => $Data
+                     );
+        echo phpver($result);
+      }
+
+
+}else{
+  $State = 520;
+  $Descriptor = 'token验证失败！';
+  $result = array (
+                   'State' => $State,
+                   'Descriptor' => $Descriptor,
+  				         'Version' => $Version,
+                   'Data' => $Data,
+                   );
+  echo phpver($result);
+}
+
+?>
